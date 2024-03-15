2b149b21ee8b89b5343350f8d091751e9c717656
honeybyhomo <32167639+honeybyhomo@users.noreply.github.com>
2024-03-15 12:05:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,20 +8,9 @@ sudo -v
 ## Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+## Install brewfile (make sure you're in a directory that has a 'brewfile' file
+brew bundle install
+
 ## Set Homebrew to automatically update
 brew autoupdate start --upgrade --cleanup --sudo
 
-## Install everything from Brewfile. Takes a while
-brew install mas
-brew install google-chrome
-brew install google-drive
-brew install microsoft-excel
-brew install microsoft-word
-brew install bitwarden
-brew install sonos
-brew install meetingbar
-brew install monitorcontrol
-brew install sublime-text
-brew install adobe-acrobat-reader
-mas install 937984704 ## Amphetamine
-mas install 1278508951 ## Trello
